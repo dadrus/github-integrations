4c322052c55e941b3b512d4af4983585f451384b
Dimitrij Drus <dadrus@gmx.de>
2017-07-12 00:25:31 +0200

    just to see sonar is used for pull request analysis

--- src/main/java/com/github/dadrus/App.java
+++ src/main/java/com/github/dadrus/App.java
@@ -6,6 +6,9 @@ package com.github.dadrus;
  */
 public class App 
 {
+	// expecting sonar to alert an issue here
+	public static final String PASSWORD = "foo";
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
