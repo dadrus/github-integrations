302bddb79ea0a74f358ca4d825ff8fc4031219a3
Dimitrij Drus <dadrus@gmx.de>
2017-07-12 18:40:03 +0200

    sonar test #2

--- build.sh
+++ build.sh
@@ -6,13 +6,12 @@ export SONAR_ORGANIZATION='dadrus-github'
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo "Building and analyzing a regular branch"
   
-  mvn clean verify sonar:sonar \
-    -Dsonar.organization=$SONAR_ORGANIZATION \
-	-Dsonar.branch=$TRAVIS_BRANCH \
-	-Dsonar.host.url=$SONAR_HOST_URL \
-	-Dsonar.login=$SONAR_TOKEN \
-	
+  export SONAR_ENV="-Dsonar.organization=$SONAR_ORGANIZATION -Dsonar.host.url=$SONAR_HOST_URL -Dsonar.login=$SONAR_TOKEN"
+  if[ "$TRAVIS_BRANCH" != "master" ]; then
+    export SONAR_ENV="$SONAR_ENV -Dsonar.branch=$TRAVIS_BRANCH"
+  fi
   
+  mvn clean verify sonar:sonar $SONAR_ENV
 else
   echo "Building and analyzing a pull request from $TRAVIS_PULL_REQUEST_BRANCH branch"
   
@@ -20,11 +19,9 @@ else
     -Dsource.skip=true \
     -Dsonar.analysis.mode=preview \
 	-Dsonar.organization=$SONAR_ORGANIZATION \
-	-Dsonar.branch=$TRAVIS_PULL_REQUEST_BRANCH \
     -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
     -Dsonar.github.repository=$TRAVIS_PULL_REQUEST_SLUG \
     -Dsonar.github.oauth=$SONAR_GITHUB_TOKEN \
     -Dsonar.host.url=$SONAR_HOST_URL \
     -Dsonar.login=$SONAR_TOKEN
-
 fi
