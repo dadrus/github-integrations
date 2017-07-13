7f13e01de5d09942741d4d9b7f67b9d2d86d5b1f
Dimitrij Drus <dadrus@gmx.de>
2017-07-13 09:56:18 +0200

    just to see the sonar results in a pr #2

--- src/main/java/com/github/dadrus/App.java
+++ src/main/java/com/github/dadrus/App.java
@@ -6,13 +6,13 @@ package com.github.dadrus;
  */
 public class App 
 {
-	public static String PASSWORD="foo"; // just to see the sonar results in a pr
+	public static final String PASSWORD="foo"; // just to see the sonar results in a pr
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
 	
 	public void test() throws Exception {
-		// just for test purposes
+		
 	}
 }
