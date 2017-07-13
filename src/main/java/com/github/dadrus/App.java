08b1169c822e62dc7ba1cd0ae44ffd5d93d6ca3b
Dimitrij Drus <dadrus@gmx.de>
2017-07-13 21:14:33 +0200

    work started on #11

--- src/main/java/com/github/dadrus/App.java
+++ src/main/java/com/github/dadrus/App.java
@@ -6,12 +6,17 @@ package com.github.dadrus;
  */
 public class App 
 {
+	public static final String PASSWORD="foo";
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
 	
 	public void test() throws Exception {
-		
+		if(true) { // sonar doesn't like this comment style
+			throw new Exception("this"); // and the raw exception anyway
+		} else {
+			throw new Exception("that");
+		}
 	}
 }
