8c1f8556595d34732c74e4dcc4d722936b0b5464
Dimitrij Drus <dimitrij.drus@innoq.com>
2023-08-21 20:37:30 +0200

    feat: SBOM

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/goccy/go-json"
+)
 
 var Version = "just test 17"
 
 func main() {
 	fmt.Println("Current Version: ", Version)
+
+	raw, _ := json.Marshal(map[string]string{"Foo": "Bar"})
+	fmt.Println("json: ", string(raw))
 }
