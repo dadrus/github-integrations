3ee0e5a3e79f897baddae11eaa4dffa9d0d9bfe3
Dimitrij Drus <dimitrij.drus@innoq.com>
2023-11-16 11:00:23 +0100

    feat: test separate repos (#160)

--- main.go
+++ main.go
@@ -2,18 +2,10 @@ package main
 
 import (
 	"fmt"
-	"maps"
-
-	"github.com/goccy/go-json"
 )
 
-var Version = "just test 17"
+var Version = "just test 18"
 
 func main() {
 	fmt.Println("Current Version: ", Version)
-
-	raw, _ := json.Marshal(map[string]string{"Foo": "Bar"})
-	fmt.Println("json: ", string(raw))
-
-	maps.Equal(map[string]string{"Foo": "Bar"}, map[string]string{"Foo": "Bar"})
 }
