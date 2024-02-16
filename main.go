acb175a67ba7c1e8662a778670c6d6f50ddaeaeb
Dimitrij Drus <dimitrij.drus@innoq.com>
2024-02-16 21:18:06 +0100

    next attempt to have suggestions

--- main.go
+++ main.go
@@ -8,4 +8,11 @@ import (
 
 func main() {
 	fmt.Println("Current Version: ", version.Version)
+
+	type Foo struct {
+		Foo string `json:"foo" yaml:"foo_bar"`
+		Bar string `json:"bar_foo" yaml:"bar"`
+	}
+
+	fmt.Println(Foo{Foo: "foo", Bar: "bar"})
 }
