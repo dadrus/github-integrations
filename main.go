edfb3854ddd11ef63b97fd87c49f1bae6f6231b1
Dimitrij Drus <dimitrij.drus@innoq.com>
2024-02-17 20:16:08 +0100

    further issues resolved

--- main.go
+++ main.go
@@ -7,14 +7,11 @@ import (
 	"github.com/dadrus/github-integrations/version"
 )
 
-func Foo(ctx context.Context) error {
+func Foo(_ context.Context) error {
 	return nil
 }
 
 func main() {
-
 	fmt.Println("Current Version: ", version.Version) //nolint:forbidigo
-
 	fmt.Println(Foo(context.TODO()))
-
 }
