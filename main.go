68d3fdcf53231db6401bacace4b3df4ac8d91331
Dimitrij Drus <dimitrij.drus@innoq.com>
2024-04-03 08:57:30 +0200

    chore: Playing around with renovate (#208)

--- main.go
+++ main.go
@@ -3,9 +3,18 @@ package main
 import (
 	"fmt"
 
+	"github.com/goccy/go-json"
+
 	"github.com/dadrus/github-integrations/version"
 )
 
 func main() {
 	fmt.Println("Current Version: ", version.Version) //nolint:forbidigo
+
+	res, err := json.Marshal(map[string]any{"foo": "bar"})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(res))
 }
